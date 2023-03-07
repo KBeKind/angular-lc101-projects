f3caee8f7938113e807b00e7316864ef79a17660
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-07 13:23:42 -0600

    Finished exercises

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,30 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie(newTitle: string){
+     
+         let upperCaseTitles = [];
+         for (let i=0; i < this.movies.length; i++){
+            upperCaseTitles.push(this.movies[i].toUpperCase());
+         }
+         let errorMsg = '';
+         if(newTitle === ''){
+            errorMsg = 'Please enter a movie title.';
+         } else if (upperCaseTitles.includes(newTitle.toUpperCase())) {
+            errorMsg = `${newTitle} is already in your movie list.`;
+         } else {
+            let newTitleArr = newTitle.split(' ');
+            for(let i = 0; i < newTitleArr.length; i++){
+               let innerArr = newTitleArr[i].split('');
+               innerArr[0] = innerArr[0].toUpperCase();
+               newTitleArr[i] = innerArr.join('');
+            }
+            newTitle = newTitleArr.join(' ');
+
+            this.movies.push(newTitle);
+         }
+         return errorMsg;
+      
+   }
 }
--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = [];
+   finishedChores = ['this','that','morstuf','Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://pt-web-dev-jan-23.github.io/js-assignment-4-html-me-something-KBeKind/images/magic-seed.png';
 
    constructor() { }
 
