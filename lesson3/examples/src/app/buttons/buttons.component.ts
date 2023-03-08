354f3afe732d363f56596b560303fae4aa539196
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-08 16:33:12 -0600

    commited the study examples too

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
+   buttonHeading: string = "Buttons";
+   jokeButtonHeading: string = "Not Trick Buttons";
    inactive: boolean = false;
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
 
+   shiftLocation(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+      }
+      return this.location;
+   }
 }
